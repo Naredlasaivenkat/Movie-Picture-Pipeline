abf78cd4ce594b7fed03b789c50ff96b997682a1
Sai Venkat <naredlasaivenkat@gmail.com>
2026-08-26 12:56:26 +0530

    Fix backend package import

--- starter/backend/__init__.py
+++ starter/backend/__init__.py
@@ -1,24 +1,18 @@
 import os
-
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
 
-from .movies import movies_api
+from movies import movies_api
 
 app = Flask(__name__)
 CORS(app)
-
 app.register_blueprint(movies_api)
-
-
-@app.route("/")
-def health():
-    return jsonify({"status": "Backend is running"})
-
 
 if __name__ == "__main__":
     app.run(
-        debug=False,
+        debug=True,
         host="0.0.0.0",
         port=int(os.getenv("FLASK_RUN_PORT", 5000)),
     )
+
+    
