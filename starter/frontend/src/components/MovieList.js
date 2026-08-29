3e3ea68e132d3ff1cc19b30c177016ddaf9cad2a
Sai Venkat <naredlasaivenkat@gmail.com>
2026-08-29 10:29:31 +0530

    Fix MovieList API data handling

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -4,17 +4,40 @@ import axios from 'axios';
 
 function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
-      setMovies(response.data.movies);
-    });
+    axios
+      .get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`)
+      .then((response) => {
+        console.log('Movie API response:', response.data);
+
+        if (Array.isArray(response.data.movies)) {
+          setMovies(response.data.movies);
+        } else {
+          setMovies([]);
+          setError('Invalid movie data received from server');
+        }
+      })
+      .catch((err) => {
+        console.error('Movie API error:', err);
+        setMovies([]);
+        setError('Unable to load movies');
+      });
   }, []);
+
+  if (error) {
+    return <p>{error}</p>;
+  }
 
   return (
     <ul>
       {movies.map((movie) => (
-        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
+        <li
+          className="movieItem"
+          key={movie.id}
+          onClick={() => onMovieClick(movie)}
+        >
           {movie.title}
         </li>
       ))}
